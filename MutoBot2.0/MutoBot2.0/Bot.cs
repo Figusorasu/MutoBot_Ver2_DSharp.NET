2dca7129435e71183fd1ffe7427fe7ac25811dc4
Figus <99464311+Figusorasu@users.noreply.github.com>
2023-04-26 17:29:12 +0200

    Initial Commit - Ver. 0.1

---
+++ MutoBot2.0/MutoBot2.0/Bot.cs
@@ -0,0 +1,76 @@
+﻿using DiscordBot.Commands;
+using DSharpPlus;
+using DSharpPlus.AsyncEvents;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.EventArgs;
+using DSharpPlus.Interactivity;
+using DSharpPlus.Interactivity.Extensions;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiscordBot
+{
+    public class Bot
+    {
+        public DiscordClient? Client { get; private set; }
+        public InteractivityExtension? Interactivity { get; private set; }
+        public CommandsNextExtension? Commands { get; private set; }
+
+
+        public async Task RunAsync()
+        {
+            var jsonConfig = string.Empty;
+
+            using (var fs = File.OpenRead("config.json"))
+            using (var sr = new StreamReader(fs, new UTF8Encoding(false)))
+
+            jsonConfig = await sr.ReadToEndAsync().ConfigureAwait(false);
+
+            var configJson = JsonConvert.DeserializeObject<ConfigJson>(jsonConfig);
+
+            var config = new DiscordConfiguration
+            {
+                Token = configJson.Token,
+                TokenType = TokenType.Bot,
+                AutoReconnect = true,
+                MinimumLogLevel = Microsoft.Extensions.Logging.LogLevel.Debug
+            };
+
+            Client = new DiscordClient(config);
+
+            Client.Ready += OnClientReady;
+
+            Client.UseInteractivity(new InteractivityConfiguration
+            { 
+               Timeout= TimeSpan.FromMinutes(2)
+            });
+
+            var commandsConfig = new CommandsNextConfiguration
+            {
+                StringPrefixes = new string[] {configJson.Prefix},
+                EnableMentionPrefix = true,
+                EnableDms = false,
+                EnableDefaultHelp = true,
+                DmHelp = false,
+                
+            };
+
+            Commands = Client.UseCommandsNext(commandsConfig);
+
+            Commands.RegisterCommands<TestCommands>();
+
+            await Client.ConnectAsync();
+            await Task.Delay(-1);
+        }
+
+        private Task OnClientReady(DiscordClient sender, ReadyEventArgs args)
+        {
+            Console.WriteLine("Bot is running");
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ MutoBot2.0/MutoBot2.0/Commands/TestCommands.cs
@@ -0,0 +1,38 @@
+﻿using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using DSharpPlus.Interactivity;
+using DSharpPlus.Interactivity.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiscordBot.Commands
+{
+    public class TestCommands : BaseCommandModule
+    {
+        [Command("ping")]
+        [Description("Returns pong")]
+        public async Task Ping(CommandContext ctx)
+        {
+            await ctx.Channel.SendMessageAsync("Pong!").ConfigureAwait(false);
+        }
+
+        [Command("add")]
+        [Description("Adds two numbers together")]
+        public async Task Add(CommandContext ctx, 
+            [Description("First number")] int numberOne, 
+            [Description("Second number")] int numberTwo)
+        {
+            await ctx.Channel.SendMessageAsync((numberOne + numberTwo).ToString()).ConfigureAwait(false);
+        }
+
+        [Command("response")]
+        public async Task Response(CommandContext ctx)
+        {
+            var interactivity = ctx.Client.GetInteractivity();
+        }
+
+    }
+}
